4ae5bc75a0f38757ea90c07ebffe0e67273b37c8
Micheal <bravomichea@gmail.com>
2020-09-01 17:09:13 +0300

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>welcome team 228</h1>
       <p>please guys lets start working on this projects as soon as possible</p>
     </div>
   );
